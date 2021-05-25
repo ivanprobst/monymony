b09af8af0044a2f44b1ecfd22eafcec6e052da5a
Ivan Probst <ivan.probst@gmail.com>
2021-05-25 11:50:19 +0200

    installed Materials-UI, created base UI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,22 @@
+// Libs
+import {ButtonGroup, Button, Grid} from '@material-ui/core';
+
 // RENDER
 export default function Header() {
   return (
     <header className="App-header">
-      <div>MonyMony</div>
-      <ul>
-        <li>Charts</li>
-        <li>Grid</li>
-        <li>Transactions</li>
-      </ul>
+      <Grid container>
+        <Grid item xs={6}>
+          <h1>MonyMony</h1>
+        </Grid>
+        <Grid item xs={6}>
+          <ButtonGroup variant="contained" color="primary">
+            <Button>Charts</Button>
+            <Button>Grid</Button>
+            <Button>Transactions</Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
     </header>
   )
 }
